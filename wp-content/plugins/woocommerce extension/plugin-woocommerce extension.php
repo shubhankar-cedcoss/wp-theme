6e97f4bbb923653167ab218f2a2f2d8746869364
shubhankar-cedcoss <shubhankarsaxena@cedcoss.com>
2020-07-13 19:00:09 +0530

    First commit

---
+++ wp-content/plugins/woocommerce extension/plugin-woocommerce extension.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Settings Tab Demo
+ * Plugin URI: http://woocommerce.com/products/woocommerce-extension/
+ * Description: This is my First WooCommmerce extension
+ * Version: 1.0.0
+ * Author: Shubhankar
+ * Author URI: http://yourdomain.com/
+ * Developer: Your Name
+ * Developer URI: http://yourdomain.com/
+ * Text Domain: woocommerce-extension
+ * Domain Path: /languages
+ *
+ * @package : woocommerce
+ *
+ * Woo: 12345:342928dfsfhsf8429842374wdf4234sfd
+ * WC requires at least: 2.2
+ * WC tested up to: 2.3
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	class WC_Settings_Tab_Demo {
+
+		/**
+		 * Bootstraps the class and hooks required actions & filters.
+		 */
+		public static function init() {
+			add_filter( 'woocommerce_settings_tabs_array', __CLASS__ . '::add_settings_tab', 50 );
+			add_action( 'woocommerce_settings_tabs_settings_tab_demo', __CLASS__ . '::settings_tab' );
+			add_action( 'woocommerce_update_options_settings_tab_demo', __CLASS__ . '::update_settings' );
+		}
+
+
+		/**
+		 * Add a new settings tab to the WooCommerce settings tabs array.
+		 *
+		 * @param array $settings_tabs Array of WooCommerce setting tabs & their labels, excluding the Subscription tab.
+		 * @return array $settings_tabs Array of WooCommerce setting tabs & their labels, including the Subscription tab.
+		 */
+		public static function add_settings_tab( $settings_tabs ) {
+			$settings_tabs['settings_tab_demo'] = __( 'Settings Demo Tab', 'woocommerce-settings-tab-demo' );
+			return $settings_tabs;
+		}
+
+
+		/**
+		 * Uses the WooCommerce admin fields API to output settings via the @see woocommerce_admin_fields() function.
+		 *
+		 * @uses woocommerce_admin_fields()
+		 * @uses self::get_settings()
+		 */
+		public static function settings_tab() {
+			woocommerce_admin_fields( self::get_settings() );
+		}
+
+
+		/**
+		 * Uses the WooCommerce options API to save settings via the @see woocommerce_update_options() function.
+		 *
+		 * @uses woocommerce_update_options()
+		 * @uses self::get_settings()
+		 */
+		public static function update_settings() {
+			woocommerce_update_options( self::get_settings() );
+		}
+
+
+		/**
+		 * Get all the settings for this plugin for @see woocommerce_admin_fields() function.
+		 *
+		 * @return array Array of settings for @see woocommerce_admin_fields() function.
+		 */
+		public static function get_settings() {
+
+			$settings = array(
+				'section_title' => array(
+					'name' => __( 'Section Title', 'woocommerce-settings-tab-demo' ),
+					'type' => 'title',
+					'desc' => '',
+					'id'   => 'wc_settings_tab_demo_section_title',
+				),
+				'title'         => array(
+					'name' => __( 'Title', 'woocommerce-settings-tab-demo' ),
+					'type' => 'text',
+					'desc' => __( 'This is some helper text', 'woocommerce-settings-tab-demo' ),
+					'id'   => 'wc_settings_tab_demo_title',
+				),
+				'description'   => array(
+					'name' => __( 'Description', 'woocommerce-settings-tab-demo' ),
+					'type' => 'textarea',
+					'desc' => __( 'This is a paragraph describing the setting. Lorem ipsum yadda yadda yadda. Lorem ipsum yadda yadda yadda. Lorem ipsum yadda yadda yadda. Lorem ipsum yadda yadda yadda.', 'woocommerce-settings-tab-demo' ),
+					'id'   => 'wc_settings_tab_demo_description',
+				),
+				'section_end'   => array(
+					'type' => 'sectionend',
+					'id'   => 'wc_settings_tab_demo_section_end',
+				),
+			);
+
+			return apply_filters( 'wc_settings_tab_demo_settings', $settings );
+		}
+
+	}
+
+	WC_Settings_Tab_Demo::init();
+}
--- wp-content/themes/poca/page-2251.php
+++ wp-content/themes/poca/page-2251.php
@@ -92,74 +92,85 @@ get_header();
       </div>
     </div>
 
-    <!-- Projects Menu -->
+    <!-- Projects Menu -->    
     <div class="container">
       <div class="poca-projects-menu mb-30 wow fadeInUp" data-wow-delay="0.3s">
         <div class="text-center portfolio-menu">
           <button class="btn active" data-filter="*">All</button>
-          <button class="btn" data-filter=".entre">Entrepreneurship</button>
+          <?php
+          $category = get_terms(array(
+                      'taxonomy' => 'Category_taxonomy',
+                      'hide_empty' => false
+          ));
+          foreach($category as $cat){
+            ?>
+          <button class="btn" data-filter="<?php echo $cat->slug; ?>"><?php echo $cat->name; ?></button>
+          <?php
+          }
+          ?>
+          <!-- <button class="btn" data-filter=".entre">Entrepreneurship</button>
           <button class="btn" data-filter=".media">Media</button>
           <button class="btn" data-filter=".tech">Tech</button>
-          <button class="btn" data-filter=".tutor">Tutorials</button>
+          <button class="btn" data-filter=".tutor">Tutorials</button> -->
         </div>
       </div>
     </div>
-    
+
     <div id="poca-portfolio" class="container">
-        <div class="row poca-portfolio">   
-        <!-- Single gallery Item -->
-        <?php
-            $args = array(  
-              'post_type'      => 'podcast',
-              'post_status'    => 'publish',
-              'posts_per_page' => 2,
-            ); 
-        
+      <div class="row poca-portfolio">
+		<?php
+		 $args = array(  
+            'post_type'      => 'podcast',
+            'post_status'    => 'publish',
+            'posts_per_page' => 2,
+        ); 
+		
             $loop = new WP_Query( $args ); 
 
             while ( $loop->have_posts() ) : $loop->the_post(); 
-            ?>  
-            <div class="col-12 col-md-6 single_gallery_item entre wow fadeInUp" data-wow-delay="0.2s"> 
-            <!-- Welcome Music Area -->
-                <div class="poca-music-area style-2 d-flex align-items-center flex-wrap">
-                    <div class="poca-music-thumbnail">
-                        <?php the_post_thumbnail(); ?>
-                    </div>
-                    <div class="poca-music-content text-center">
-                        <span class="music-published-date mb-2"><?php echo get_the_date(); ?></span>
-                        <h2><?php the_title(); ?></h2>
-                       <?php
-                       $list = wp_get_post_terms($post->ID, 'Category_taxonomy', array( 'fields' => 'all' ) );
-                       //print_r($list);
-                       foreach($list as $taxonomy) {           
-                        ?>   
-                        <div class="music-meta-data">
-                            <p>By <a href="#" class="music-author"><?php the_author(); ?></a> | <a href="<?php echo $taxonomy->slug ?>" class="music-catagory"><?php echo $taxonomy->name; ?></a> | <a href="#" class="music-duration">00:02:56</a></p>
-                        </div>
-                       <?php } ?>
-                        <!-- Music Player -->
-                        <div class="poca-music-player">
-                            <audio preload="auto" controls>
-                            <source src="<?php echo get_template_directory_uri(); ?>/audio/dummy-audio.mp3">
-                            </audio>
-                        </div>
-                        <!-- Likes, Share & Download -->
-                        <div class="likes-share-download d-flex align-items-center justify-content-between">
-                            <a href="#"><i class="fa fa-heart" aria-hidden="true"></i> Like (29)</a>
-                            <div>
-                            <a href="#" class="mr-4"><i class="fa fa-share-alt" aria-hidden="true"></i> Share(04)</a>
-                            <a href="#"><i class="fa fa-download" aria-hidden="true"></i> Download (12)</a>
-                        </div>
-                        </div>
-                    </div>                    
-                </div>
-            </div>
-            <?php
-            endwhile;
-
-            wp_reset_postdata();
-            ?>
+            ?>    
+				<!-- Single gallery Item -->
+				<div class="col-12 col-md-6 single_gallery_item entre wow fadeInUp" data-wow-delay="0.2s">
+				<!-- Welcome Music Area -->
+				<div class="poca-music-area style-2 d-flex align-items-center flex-wrap">
+					<div class="poca-music-thumbnail">
+					<?php the_post_thumbnail(); ?>
+					</div>
+					<div class="poca-music-content text-center">
+					<span class="music-published-date mb-2"><?php echo get_the_date(); ?></span>
+          <h2><?php the_title(); ?></h2>
+          <?php
+          $list = wp_get_post_terms($post->ID, 'Category_taxonomy', array( 'fields' => 'all' ) );
+          //print_r($list);
+          foreach($list as $taxonomy) {           
+          ?>   
+					<div class="music-meta-data">
+						<p>By <a href="#" class="music-author"><?php the_author(); ?></a> | <a href="#" class="music-catagory"><?php echo $taxonomy->name ?></a> | <a href="#" class="music-duration"><?php the_time(); ?></a></p>
+          </div>
+          <?php } ?>
+					<!-- Music Player -->
+					<div class="poca-music-player">
+						<audio preload="auto" controls>
+						<source src="<?php echo get_template_directory_uri(); ?>/audio/dummy-audio.mp3">
+						</audio>
+					</div>
+					<!-- Likes, Share & Download -->
+					<div class="likes-share-download d-flex align-items-center justify-content-between">
+						<a href="#"><i class="fa fa-heart" aria-hidden="true"></i> Like (29)</a>
+						<div>
+						<a href="#" class="mr-4"><i class="fa fa-share-alt" aria-hidden="true"></i> Share(04)</a>
+						<a href="#"><i class="fa fa-download" aria-hidden="true"></i> Download (12)</a>
+						</div>
+					</div>
+					</div>
+				</div>
         </div>
+        <?php
+        endwhile;
+
+        wp_reset_postdata();
+        ?>
+      </div>
     </div>
 
     <div class="container">
